a9f3608a7ef3a247368adbe0b195ca879ee81cc5
fendyIskandar <fendyaremania87@gmail.com>
2022-03-02 11:18:43 -0800

    konverter done

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // ignore: non_constant_identifier_names
   TextEditingController Input = TextEditingController();
+
+  double _inputUser = 0;
+  double _kelvin = 0;
+  double _reamur = 0;
+
+  void konversi() {
+    setState(
+      () {
+        _inputUser = double.parse(Input.text);
+        _kelvin = _inputUser + 273;
+        _reamur = _inputUser * 0.8;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,23 +36,78 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Konverter Suhu"),
+          centerTitle: true,
+          title: const Text("Konverter suhu"),
         ),
         body: Container(
-            margin: const EdgeInsets.all(8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                      hintText: "Masukkan Suhu Dalam Celcius"),
-                  controller: Input,
-                  keyboardType: TextInputType.number,
-                )
-              ],
-            )),
+          margin: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Masukkan Suhu Dalam Celcius"),
+                textAlign: TextAlign.center,
+                controller: Input,
+                keyboardType: TextInputType.number,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      const Text(
+                        "Suhu Dalam Kelvin",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w200),
+                      ),
+                      Text(
+                        "$_kelvin",
+                        style: const TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text(
+                        "Suhu Dalam Reamur",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w200,
+                        ),
+                      ),
+                      Text(
+                        "$_reamur",
+                        style: const TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 45,
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: konversi,
+                  child: const Text(
+                    "Konversi Suhu",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
